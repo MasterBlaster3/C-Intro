1096711c8290f69e20e75cc2b800a65f7ccf86f5
Joe Maifeld <josephmaifeld@gmail.com>
2022-08-26 12:03:56 -0500

    First C# file!

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// This is a C# source file. It's the file that holds your C# source code. (note the .cs file extension) Think of it as your main.js. It's where the logic of your application starts.
+
+// using System;
+
+// namespace Intro
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+
+// System.Console.WriteLine("Hello World!");
+
+// System.Console.Write("Who would you like to say hello to? ");
+
+// string name = System.Console.ReadLine();
+
+// if (string.IsNullOrWhiteSpace(name))
+// {
+//     System.Console.WriteLine("Fine, don't say 'hello'!");
+// }
+// else
+// {
+//     System.Console.WriteLine($"Hello, {name}!");
+// }
+
+using System;
+
+Console.Write("Who would you like to say hello to? ");
+
+string name = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    Console.WriteLine($"Hello, {name}!");
+}
